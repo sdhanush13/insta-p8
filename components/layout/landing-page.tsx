848d93b5d97a3eb5d1caa37aaa97c9f09bc596c7
Dhanush Suresh <dhanush@micglobal.com>
2026-06-09 11:25:35 +0530

    minimal instagram scope test

--- components/layout/landing-page.tsx
+++ components/layout/landing-page.tsx
@@ -5,7 +5,7 @@ import { Zap, MessageCircle, Shield, Clock } from "lucide-react"
 
 export function LandingPage() {
   const handleLogin = () => {
-    window.location.href = `https://www.instagram.com/oauth/authorize?force_reauth=true&client_id=${process.env.NEXT_PUBLIC_INSTAGRAM_APP_ID}&redirect_uri=${process.env.NEXT_PUBLIC_INSTAGRAM_REDIRECT_URI}&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish%2Cinstagram_business_manage_insights`
+    window.location.href = `https://www.instagram.com/oauth/authorize?force_reauth=true&client_id=${process.env.NEXT_PUBLIC_INSTAGRAM_APP_ID}&redirect_uri=${process.env.NEXT_PUBLIC_INSTAGRAM_REDIRECT_URI}&response_type=code&scope=instagram_business_basic`
   }
 
   return (
